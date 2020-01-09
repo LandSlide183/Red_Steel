22df7f5f026a0fed5fa0353869c3aba612481aec
LandSlide183 <josh.huddleston@icloud.com>
2020-01-08 20:24:18 -0500

    Changing everything over to IDEA

---
+++ src/main/java/com/landslide/redsteelmod/RedSteelMain.java
@@ -0,0 +1,10 @@
+package com.landslide.redsteelmod;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(RedSteelMain.MODID)
+public final class RedSteelMain {
+
+	public static final String MODID = "redsteelmod";
+
+}
